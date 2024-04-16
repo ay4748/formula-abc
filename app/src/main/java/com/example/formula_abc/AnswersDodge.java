11aa7f457308a4fc538240f6977ef77249b4cdf9
ay4748 <ay4748@bs.amalnet.k12.il>
2024-04-16 21:48:56 +0300

    dodge

---
+++ app/src/main/java/com/example/formula_abc/AnswersDodge.java
@@ -0,0 +1,86 @@
+package com.example.formula_abc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+public class AnswersDodge extends AppCompatActivity {
+    int a, b, c;
+    int answers = 0;
+
+    WebView wV;
+    TextView tV;
+
+   
+    String finalAnswer;
+    Intent intent;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answers_dodge);
+
+        intent = getIntent();
+
+        wV = findViewById(R.id.wV);
+
+        wV.getSettings().setJavaScriptEnabled(true);
+        wV.setWebViewClient(new MyWebViewClient());
+        wV.getSettings().setUseWideViewPort(true);
+        wV.getSettings().setLoadWithOverviewMode(true);
+
+        tV = findViewById(R.id.ansDodge1);
+
+        a = intent.getIntExtra("first", 0);
+        b = intent.getIntExtra("second", 0);
+        c = intent.getIntExtra("third", 0);
+
+        String url = "https://mathforyou.net/en/online/equation/arbitrary/?e0=" + a + "x%5E2%2B" + b + "*x%2B" + c + "&v0=x&o0=1&from=google";
+        wV.loadUrl(url);
+
+
+        double underShorsh = b * b - 4 * a * c;
+
+        if (underShorsh < 0) 
+        {
+            finalAnswer = "no answers";
+        } 
+        else if (underShorsh == 0) 
+        {
+            double answer = -b / (2 * a);
+            finalAnswer = "only one answer: " + answer;
+        } 
+        else 
+        {
+            double answer1 = (-b + Math.sqrt(underShorsh)) / (2 * a);
+            double answer2 = (-b - Math.sqrt(underShorsh)) / (2 * a);
+            finalAnswer = "first answer is: " + answer1 + "\nthe second answer is: " + answer2;
+        }
+        
+        tV.setText(finalAnswer);
+    }
+
+
+    public void goback(View view) {
+
+        intent.putExtra("answers", finalAnswer);
+        setResult(RESULT_OK, intent);
+
+        finish();
+    }
+
+
+    private class MyWebViewClient extends WebViewClient {
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+    }
+}
--- app/src/main/java/com/example/formula_abc/MainActivity.java
+++ app/src/main/java/com/example/formula_abc/MainActivity.java
@@ -1,14 +1,95 @@
 package com.example.formula_abc;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    Random rnd = new Random();
+    EditText aPar,bPar,cPar;
+    String answers;
+    TextView tV;
+    final int REQUEST_CODE = 1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aPar = findViewById(R.id.aDodge);
+        bPar = findViewById(R.id.bDodge);
+        cPar = findViewById(R.id.cDodge);
+
+        tV = findViewById(R.id.answersDodge);
+
     }
+
+    public void randomDodge(View view)
+    {
+        int a = rnd.nextInt(10) + 1;
+        int b = rnd.nextInt(10) + 1;
+        int c = rnd.nextInt(10) + 1;
+
+        aPar.setText(a + "");
+        bPar.setText(b + "");
+        cPar.setText(c + "");
+    }
+
+
+    public boolean canAct(String st)
+    {
+        if (st.isEmpty() || st.equals("-") || st.equals(".") || st.equals("+") || st.equals("-.") || st.equals("+.") )
+        {
+            Toast.makeText(this, "Invalid input", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public void ansDodge(View view)
+    {
+        String firstPar = aPar.getText().toString();
+        String secPar = bPar.getText().toString();
+        String thirdPar = cPar.getText().toString();
+
+        if (canAct(firstPar) && canAct(secPar) && canAct(thirdPar)) {
+            int a = Integer.parseInt(firstPar);
+            if (a == 0)
+            {
+                Toast.makeText(this, "cant put 0 there lil bro", Toast.LENGTH_LONG).show();
+            }
+            int b = Integer.parseInt(secPar);
+            int c = Integer.parseInt(thirdPar);
+
+            Intent intent = new Intent(this, AnswersDodge.class);
+            intent.putExtra("first", a);
+            intent.putExtra("second", b);
+            intent.putExtra("third", c);
+
+            startActivityForResult(intent,1234);
+        }
+    }
+
+    protected void onActivityResult(int source, int result, @Nullable Intent data_back)
+    {
+        super.onActivityResult(source, result, data_back);
+        if (data_back != null)
+        {
+            String answer = data_back.getStringExtra("answers");
+            tV.setText(answer);
+        }
+
+    }
+
 }
